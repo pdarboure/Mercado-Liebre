d0b2bcfdd344ad8b13d5195fd597ce1586278554
Pedro Agustin Darboure <119804246+pdarboure@users.noreply.github.com>
2023-01-23 20:08:23 -0300

    Update productos.js

--- routes/productos.js
+++ routes/productos.js
@@ -3,3 +3,4 @@ const productoscontrollers = require("../controllers/productosController")
 app.get("/", productoscontrollers.index);
 app.get("/login", productoscontrollers.login);
 app.get("/register", productoscontrollers.register);
+
